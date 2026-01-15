c938afbce07e5b02106abb9a59bdf797e9f53540
Fisraiagan <134463133+Fisraiagan@users.noreply.github.com>
2026-01-14 19:56:13 -0500

    Enhance connect.php with comments for clarity
    Added comments to clarify database connection setup.

--- connect.php
+++ connect.php
@@ -1,14 +1,17 @@
 <?php
 declare (strict_types=1);
-//1. Set Up & Start
 
+
+/* Database connection details */
 $host="localhost";
 $dbname="connection_test";
 $user ="root";
 $pass ="";
 
+/* Data Source Name */
 $dsn="mysql:host=$host;dbname=$dbname";
 
+/* Create PDO instance */
 try{
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO ::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
